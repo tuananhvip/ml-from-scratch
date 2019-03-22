74986e3b1fa59734123495a2b395d9bc708e9093
tgiang <giangtran240896@gmail.com>
2019-03-22 13:50:53 +0700

    Update

--- svm/svm.py
+++ svm/svm.py
@@ -1,5 +1,15 @@
-from utils import load_mat_file, plot_data
+from utils import load_mat_file, Graph
 from sklearn.svm import SVC
 
 X, y = load_mat_file('ex6data1.mat')
-plot_data(X, y)
+plot = Graph(X, y, 'on')
+plot.plot_data()
+
+C = 1.0
+linear_svm = SVC(C=C, kernel='linear')
+
+linear_svm.fit(X, y)
+
+W = linear_svm.coef_
+b = linear_svm.intercept_
+plot.visualize_boundary(W, b)
--- svm/utils.py
+++ svm/utils.py
@@ -1,16 +1,31 @@
 from scipy import io
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def load_mat_file(mat_file):
     mat = io.loadmat(mat_file)
     return mat['X'], mat['y']
 
+class Graph:
 
-def plot_data(X, y):
-    y = y.reshape((-1, ))
-    class_1 = X[y == 0]
-    class_2 = X[y == 1]
-    plt.scatter(class_1[:, 0], class_1[:, 1], c='r', marker='+')
-    plt.scatter(class_2[:, 0], class_2[:, 1], c='b', marker='o')
-    plt.show()
+    def __init__(self, X, y, interactive='off'):
+        if interactive == 'on':
+            plt.ion()
+        self.X = X
+        self.y = y
+
+    def plot_data(self):
+        y = self.y.reshape((-1, ))
+        class_1 = self.X[y == 0]
+        class_2 = self.X[y == 1]
+        plt.scatter(class_1[:, 0], class_1[:, 1], c='r', marker='+')
+        plt.scatter(class_2[:, 0], class_2[:, 1], c='b', marker='o')
+        plt.show()
+
+    def visualize_boundary(self, W, b):
+        W = W.T.reshape((2, ))
+        xp = np.linspace(np.min(self.X[:, 0]), np.max(self.X[:, 0]), 100)
+        yp = -(W[0] * xp + b)/W[1]
+        plt.plot(xp, yp)

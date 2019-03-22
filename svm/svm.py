1c54bb228f85477a2747ebeba5bc62c54b0aac82
tgiang <giangtran240896@gmail.com>
2019-03-22 16:50:14 +0700

    Update SVM

--- svm/svm.py
+++ svm/svm.py
@@ -1,15 +1,26 @@
 from utils import load_mat_file, Graph
 from sklearn.svm import SVC
 
-X, y = load_mat_file('ex6data1.mat')
-plot = Graph(X, y, 'on')
-plot.plot_data()
 
-C = 1.0
-linear_svm = SVC(C=C, kernel='linear')
+def linear_kernel():
+    X, y = load_mat_file('ex6data1.mat')
+    plot = Graph(X, y, 'on')
+    plot.plot_data()
 
-linear_svm.fit(X, y)
+    C = 1.0
+    linear_svm = SVC(C=C, kernel='linear')
 
-W = linear_svm.coef_
-b = linear_svm.intercept_
-plot.visualize_boundary(W, b)
+    linear_svm.fit(X, y)
+
+    W = linear_svm.coef_
+    b = linear_svm.intercept_
+    plot.visualize_boundary(W, b)
+
+
+def brf_kernel():
+    X, y = load_mat_file('ex6data2.mat')
+    plot = Graph(X, y, 'on')
+    plot.plot_data()
+
+
+brf_kernel()

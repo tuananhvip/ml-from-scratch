95e28cd81251c7ba38bb9f0c756546e078893122
giangtranml <giangtran240896@gmail.com>
2019-04-22 23:02:43 +0700

    Update Lenet model

--- projects/project_1/advanced/cnn.py
+++ projects/project_1/advanced/cnn.py
@@ -17,18 +17,21 @@ class Lenet:
         self.fc_layers()
 
     def train(self, X_train, y_train):
+        X_train = tf.cast(X_train, tf.float32)
+        print(tf.shape(X_train))
         for e in range(self.epoch):
             with tf.GradientTape() as tape:
                 logits = self.model(inputs=X_train, training=True)
                 loss_value = self.loss_function(y_train, logits)
             grads = tape.gradient(loss_value, self.model.trainable_variables)
             self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+            print(loss_value)
 
     def conv_layers(self):
-        self.model.add(tf.keras.layers.Conv2D(filters=6, kernel_size=(5, 5), activation='tanh'))
-        self.model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=2))
-        self.model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), activation='tanh'))
-        self.model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=2))
+        self.model.add(tf.keras.layers.Conv2D(input_shape=(28, 28, 1), filters=6, kernel_size=(5, 5), activation='tanh', padding='same'))
+        self.model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=2, padding='same'))
+        self.model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), activation='tanh', padding='same'))
+        self.model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=2, padding='same'))
 
     def fc_layers(self):
         self.model.add(tf.keras.layers.Dense(units=120, activation='tanh'))
@@ -43,5 +46,11 @@ if __name__ == '__main__':
 
     images, labels = mndata.load_training()
 
+    scaler = StandardScaler()
+    scaler.fit(images)
+    images = scaler.transform(images)
+
+    images = images.reshape((-1, 28, 28, 1))
+
     lenet = Lenet(100, 64, tf.keras.optimizers.SGD, tf.keras.losses.categorical_crossentropy)
-    lenet.train()
+    lenet.train(images, labels)

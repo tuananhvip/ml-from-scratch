5b5d989e146e37960996e9281ba2a3727acc1e5c
unknown <giangtran240896@gmail.com>
2019-07-05 09:15:16 +0700

    Add sample keras in RNN

--- recurrent_neural_network/main_rnn.py
+++ recurrent_neural_network/main_rnn.py
@@ -1,16 +1,18 @@
 import sys
 sys.path.append("..")
-sys.path.append("/home/james/Desktop/ml-from-scratch")
+# sys.path.append("/home/james/Desktop/ml-from-scratch")
 from recurrent_neural_network import RNN
 import numpy as np
 from keras.utils.np_utils import to_categorical
 from optimizations_algorithms.optimizers import SGD
+import keras
+from keras import layers as L
 
 def main():
     start_token = " "
     pad_token = "#"
 
-    with open("/home/james/Desktop/ml-from-scratch/recurrent_neural_network/names") as f:
+    with open("names") as f:
         names = f.read()[:-1].split('\n')
         names = [start_token + name for name in names]
     print('number of samples:', len(names))
@@ -44,19 +46,32 @@ def main():
         return names_ix
         
     matrix_sequences = to_matrix(names)
+    
+    train_X = matrix_sequences[:, :-1]
     m, length = matrix_sequences.shape
     input_sequences = np.zeros(shape=(m, length, n_tokens))
     for i in range(m):
         input_sequences[i] = to_categorical(matrix_sequences[i], n_tokens, dtype='int32')
     del matrix_sequences
-    train_X = input_sequences[:, :-1, :]
     train_Y = input_sequences[:, 1:, :]
 
-    optimizer = SGD()
-    epochs = 2
+    # optimizer = SGD()
+    # epochs = 2
 
-    rnn = RNN(hidden_units=64, epochs=epochs, optimizer=optimizer)
-    rnn.train(train_X, train_Y)
+    # rnn = RNN(hidden_units=64, epochs=epochs, optimizer=optimizer)
+    # rnn.train(train_X, train_Y)
+    model = keras.models.Sequential()
+    model.add(L.InputLayer([None],dtype='int32'))
+    model.add(L.Embedding(MAX_LENGTH, n_tokens))
+    model.add(L.SimpleRNN(64,return_sequences=True))
+
+    #add top layer that predicts tag probabilities
+    stepwise_dense = L.Dense(n_tokens,activation='softmax')
+    stepwise_dense = L.TimeDistributed(stepwise_dense)
+    model.add(stepwise_dense)
+
+    model.compile('adam','categorical_crossentropy', metrics=['acc'])
+    model.fit(x=train_X, y=train_Y, epochs=10)
 
 
 if __name__ == "__main__":

81ed64b76b7be731df1dddeb1c13772b2e0e6d61
unknown <giangtran240896@gmail.com>
2019-07-10 12:49:32 +0700

    Update CNN

--- convolutional_neural_network/main_cnn.py
+++ convolutional_neural_network/main_cnn.py
@@ -1,21 +1,14 @@
 import sys
 sys.path.append("..")
-debug = False
-debug_path = "D:\ml_from_scratch"
-if debug:
-    sys.path.append(debug_path)
+
 from libs.utils import load_dataset_mnist, preprocess_data
 from libs.mnist_lib import MNIST
 from optimizations_algorithms.optimizers import SGD, SGDMomentum, RMSProp, Adam
 from convolutional_neural_network import CNN
 
 def main():
-    if debug:
-        load_dataset_mnist(debug_path + "/libs")
-        mndata = MNIST(debug_path + "/libs/data_mnist")
-    else:
-        load_dataset_mnist("../libs")
-        mndata = MNIST('../libs/data_mnist')
+    load_dataset_mnist("../libs")
+    mndata = MNIST('../libs/data_mnist')
     lenet_arch = [{"type": "conv", "filter_size": (5, 5), "filters": 6, "padding": "SAME", "stride": 1, "activation": "sigmoid", "batch_norm": None},
                 {"type": "pool", "filter_size": (2, 2), "stride": 2, "mode": "max"},
                 {"type": "conv", "filter_size": (5, 5), "filters": 16, "padding": "SAME", "stride": 1, "activation": "sigmoid", "batch_norm": None},
@@ -28,8 +21,7 @@ def main():
     epochs = 20
     batch_size = 32
     learning_rate = 0.1
-    # sgd = SGD(learning_rate)
-    optimizer = SGDMomentum(alpha=learning_rate)
+    optimizer = SGD(alpha=learning_rate)
     cnn = CNN(epochs=epochs, batch_size=batch_size, optimizer=optimizer, cnn_structure=lenet_arch)
     training_phase = True
     if training_phase:
